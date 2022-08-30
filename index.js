8ed6c2b8dd345be2f8640ffd9b2766f662253bc0
stvnlee890 <stvnlee890@gmail.com>
2022-08-29 17:34:20 -0700

    changing placement of cors

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const cors = require('cors');
 app.set('port', process.env.PORT || 8080);
 
 // MIDDLEWARE
-app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 // ROUTES
 app.get('/', (req, res) => {
