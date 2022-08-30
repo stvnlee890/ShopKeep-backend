58bb2dfda7df91c231132a5b159dad320ddd1775
stvnlee890 <stvnlee890@gmail.com>
2022-08-29 17:31:24 -0700

    reinstalled bcrypt to see what happens

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
 const { createUserToken } = require('../middleware/auth')
 
 // GET ALL USERS 'user/
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User')
 require('dotenv').config()
